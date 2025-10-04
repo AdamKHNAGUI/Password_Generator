69294cc5f69c416349cbc6cb4161edce264f9c98
Adam KHNAGUI <khnagui.adam@gmail.com>
2025-10-04 09:09:30 +0200

    Générateur de mot de passe version console

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,21 @@
-use std::env; //lib pour lire les arguments de la commande
-use std::fs; // lib pour manipuler les fichiers
+use rand::{distributions::Alphanumeric, Rng}; //lib pour générer des valeurs aléatoires
+use std::env;
 
 fn main() {
-    // variable immuable de vecteur String qui récupère toute la commande et ses paramètres
-   let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-    //on vérifie si on a un argument pour le programme
-    if args.len() < 2 {
-        println!("Renseignez un fichier en paramètre : <file.txt>");
-        return;
-    } else if args.len() > 2 {
-        println!("Renseignez un seul fichier en paramètre");
-        return;
-    }
+    // on vérifie si il y a un paramètre on le convertit en usize, valeur de 12 si pas de paramètre ou problème de conversion
+    let length = if args.len() > 1 {
+        args[1].parse::<usize>().unwrap_or(12)
+    } else {
+        12
+    };
 
-    // on récupère le fichier en paramètre
-    let filename = &args[1];
+    let password: String = rand::thread_rng()          //générateur aléatoire sécurisé pour le thread courant
+        .sample_iter(&Alphanumeric) //crée un flux de caractères alphanumériques
+        .take(length)               //garder le nombre de caractère demandé
+        .map(char::from)           //convertit chaque bit en caractère
+        .collect();                                    //récupère tous les caractères dans la variable String
 
-    // on lit le conetnue du fichier , renvoie un message d'erreur en cas de problème
-    let content = fs::read_to_string(filename).expect("Erreur Lecture du fichier");
-
-    // on récupère les mots en retirants les espaces, tabulations,etc...
-    let words: Vec<&str> = content.split_whitespace().collect();
-
-    // on récupère le nombre de lettre en comptant chauqe caractère du texte sans prendre en compte les espaces, tabulations, etc...
-    let char = content.chars().filter(|c| c.is_alphabetic()).count();
-
-    // on affiche le nombre de mots et de lettre dans le fichier
-    println!("Nombre de mots : {}", words.len());
-    println!("Nombre de lettre : {}", char);
+    println!("Mot de passe généré: {}", password);
 }
